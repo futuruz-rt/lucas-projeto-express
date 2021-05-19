7ccbac6af859fd309ca7b7e50285705d135b4da5
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-05-19 18:47:15 -0300

    Inserção de dados de Cidade através da conta admin

---
+++ model/Cidades.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+var mongoURL = 'mongodb://127.0.0.1:27017/projetoweb2'
+
+module.exports = class Cidades{
+	static async cadastrar(nomeCidade,estado,descricao,imagem){
+		const conn = await MongoClient.connect(mongoURL);
+		const db = conn.db();
+		let correto = 0;
+
+		let cidadesArray =  await db.collection('cidades').find({ nomeCidade:nomeCidade,estado:estado,descricao:descricao}).toArray();
+		if (cidadesArray.length===0 && nomeCidade !== "" && estado !== "" && descricao!== ""){
+			db.collection('cidades').insertOne({nomeCidade:nomeCidade,estado:estado,descricao:descricao,imagem:imagem});
+			correto=1;
+		}
+		conn.close();
+		return correto;
+	}
+
+	static async buscar(busca){
+		const conn = await MongoClient.connect(mongoURL);
+		const db = conn.db();
+		let result = null;
+
+		if(busca){
+			result =  await db.collection('cidades').find({ nomeCidade: new RegExp('^' + busca)} ).toArray();
+		}
+		conn.close();
+		return result;
+	}
+}
