b8b5e5515b984e1ef644360a9027f1ae39fdc64c
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-05-20 19:50:09 -0300

    Configuração cloud

--- model/Users.js
+++ model/Users.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-var mongoURL = 'mongodb://127.0.0.1:27017/projetoweb2'
+var mongoURL = 'mongodb+srv://projetoweb2:6GrGdPRIjlJZJmoe@projetoweb2.edtud.mongodb.net/projetoweb2?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/projetoweb2'
 
 module.exports = class Users{
 	static async login(usuario,senha){
@@ -7,7 +7,7 @@ module.exports = class Users{
 		const db = conn.db();
 		let result =  await db.collection('users').find({usuario:usuario,senha:senha}).toArray();		
 		conn.close();
-		return result.length
+		return result.length;
 	}
 
 	static async cadastrar(email,usuario,senha){
@@ -19,7 +19,7 @@ module.exports = class Users{
 		let result = usuarioArray.length + emailArray.length
 
 		if (usuarioArray.length === 0 && emailArray.length === 0 && email !=="" && usuario!=="" && senha!== ""){
-			db.collection('users').insertOne({email: email,usuario:usuario,senha:senha,isAdmin:0});
+			db.collection('users').insertOne({email:email,usuario:usuario,senha:senha,isAdmin:0});
 		}
 		conn.close();
 		return result;
